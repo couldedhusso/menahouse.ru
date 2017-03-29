56cd1fac4c1a4faf36fa4c84f393f44af6e9ab57
couldedhusso <husseincoulibaly@gmail.com>
2017-03-29 15:33:31 +0300

    cache server

--- config/database.php
+++ config/database.php
@@ -113,7 +113,17 @@ return [
 
     'redis' => [
 
-        'cluster' => false,
+        'client' => 'predis',
+
+        'default' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 0,
+        ],
+
+
+        // 'cluster' => false,
 
         // 'default' => [
         //             'host'     => env('REDIS_HOST'),
